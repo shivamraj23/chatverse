5dc1130e6b25db4fb72a24541fe0055d1893b94a
Shivam Raj <125357309+shivamraj23@users.noreply.github.com>
2023-09-28 01:17:37 +0530

    firebase apikey protected

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDg6N88Mv38K2FaYZt_lXV3SU__IRG4pOc",
-  authDomain: "chat-verse-9e4ba.firebaseapp.com",
-  projectId: "chat-verse-9e4ba",
-  storageBucket: "chat-verse-9e4ba.appspot.com",
-  messagingSenderId: "370683359806",
-  appId: "1:370683359806:web:ac0d677df490a592d8176f",
-  measurementId: "G-TYDYW6LS30",
-};
+//private data
+//lookout for apikey on firebase dashboard
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
